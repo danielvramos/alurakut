0f154404ca2355492c7f1b7610851f3c67efcbf0
Daniel Vieira Ramos <vieira.ramos@gmail.com>
2021-07-13 14:29:39 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,33 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from '../src/lib/AlurakutCommons';
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+
+/* Reset CSS (Necolas Reset CSS <3) */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: sans-serif;
+  background-image: url("https://www.viagenspossiveis.com.br/wp-content/uploads/2019/09/Praia-do-Aventureiro-Ilha-Grande-RJ-39.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-color: #D9E6F6;
+}
+#__next {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+img {
+  max-width: 100%;
+  height: auto;
+  display: block;
+}
+${AlurakutStyles}
+
 `
 
 const theme = {
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,131 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu,AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
+import React from 'react';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidebar(propriedades) {
+  
+  return (
+    <Box>
+      <img src={`https://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px' }} />
+      <hr/>
+      <p>
+        <a className='boxlink' href={`https://github.com/${propriedades.githubUser}`}>
+          @{propriedades.githubUser}
+        </a>
+      </p>
+      <hr/>
+      <AlurakutProfileSidebarMenuDefault />
+    </Box>
+  )
+}
+function RelationsBox(props){
+  return (
+            <> 
+              <h2 className="smallTitle">
+               {props.title}
+              </h2>
+
+              <ul>
+                {props.arrayFavoritos.slice(0,6).map((itemAtual) => {
+                  return (
+                    
+                      <li key={itemAtual.id}>
+                        <a href={`/users/${itemAtual.name}`} >
+                          <img src={itemAtual.image} />
+                          <span>{itemAtual.title}</span>
+                        </a>
+                     </li>
+                  )
+                })}
+              </ul>
+            </>
+          )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const usuarioAleatorio = 'danielvramos';
+  const [comunidades, setComunidades] = React.useState([{
+    id: new Date().toISOString(),
+    name: 'eu odeio acordar cedo',
+    image: 'https://w7.pngwing.com/pngs/969/123/png-transparent-garfield-minus-garfield-jon-arbuckle-odie-comics-good-night-miscellaneous-sleep-cartoon.png'
+  }])
+
+  const pessoas = [
+    'juunegreiros',
+    'omariosouto',
+    'peas',
+    'rafaballerini',
+    'marcobrunodev',
+    'felipefialho',
+    'guilhermesilveira'
+  ].map((itemAtual) =>{
+
+    return (
+     {id: new Date().toISOString()+itemAtual,
+      name:itemAtual,
+    image:'https://github.com/'+itemAtual+'.png'
+    } 
+    )
+  })
+return (
+    <>    
+      <AlurakutMenu githubUser={usuarioAleatorio}/>
+      <MainGrid>
+        {/* <Box style="grid-area: profileArea;"> */}
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className="title">
+              Bem vindo(a) 
+            </h1>
+
+            <OrkutNostalgicIconSet />
+          </Box>
+          <Box>
+            <h2 className='subtitle'>O que você deseja fazer?</h2>
+            <form onSubmit={function handlerCreateCommunity(e){
+              e.preventDefault()
+              
+              const dadosDoForm = new FormData(e.target);
+              const comunidade = {
+                id:new Date().toISOString(),
+                title: dadosDoForm.get("title"),
+                image: dadosDoForm.get("image")
+              }
+
+              const comunidadesAtual = [...comunidades,comunidade]
+
+              setComunidades(comunidadesAtual)
+            }}>
+              <input type='text'
+              placeholder='Qual vai ser o nome de sua comunidade?'
+              aria-label='Qual vai ser o nome de sua comunidade?'
+              name ="title"/>
+
+              <input type='text'
+              placeholder='informe URL para ser usada como capa'
+              aria-label='informe URL para ser usada como capa'
+              name ="image"/>
+
+              <button>
+                Salvar comunidade
+              </button>               
+            </form>
+          </Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <RelationsBox arrayFavoritos={pessoas} title= {`Pessoas da comunidade ( ${pessoas.length} )`}/>
+          </ProfileRelationsBoxWrapper>
+          <ProfileRelationsBoxWrapper>
+            <RelationsBox arrayFavoritos={comunidades} title= {`Comunidades ( ${comunidades.length} )`}/>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  )
 }
